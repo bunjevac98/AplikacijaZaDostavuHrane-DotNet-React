c22ec98d621e04545107286e7e4f694282d86cb7
bunjevac98 <basic.aco@hotmail.com>
2023-02-16 23:34:41 +0100

    Dodat bootstrap i povezivanje sa Serverom

--- reactclient/src/App.js
+++ reactclient/src/App.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 export default function App() {
+  const [korisnici, setKorisnici] = useState([])
+
+  function getKorisnici() {
+    const url = "https://localhost:7220//get-all-korisnici";
+    //OVO NAM KAZE KOJU CEMO 
+    fetch(url, {
+      method: 'GET'
+    })
+      .then(response => response.json)
+      .then(korisniciFromServer => {
+        console.log(korisniciFromServer);
+        setKorisnici(korisniciFromServer);
+      })
+      .catch((error => {
+        console.log(error);
+        alert(error);
+      }));
+  }
+
   return (
-    <div>
-      HELLLLLLOOOOOOOOOOOOOOOOO!!!!
+    <div class="nav-bar">
+      <h1>
+        HELOO WORLD
+      </h1>
+      
+
+
+
+
     </div>
   );
+
+  function renderKorisniciTable() {
+    return (
+      <div>
+
+      </div>
+    )
+
+
+  }
+
+
 }
